5ab0ed7706fa90b2522cb17331a2e9276d6fae60
Fernando Lopez <fer.neutron@gmail.com>
2019-12-01 21:30:44 -0600

    initializing train test split function

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,17 @@
 import networkx as nx
+from random import randint
 
 class PrepareGraph:
    def __init__(self, **kwargs):
       self.Graph = PrepareGraph.load_graph(kwargs['file'])
       self.adjacency = nx.adjacency_matrix(self.Graph)
-      self.train, self.test = PrepareGraph.train_test_split(self.adjacency, kwargs['test_size'])
+      self.train, self.test = PrepareGraph.train_test_split(self.adjacency.copy(), kwargs['test_size'])
       
    @staticmethod
    def train_test_split(adjacency, test_size):
-      print(type(adjacency))
+      test_items = round(adjacency.shape[0] * test_size)
+      
+      return 1,2
       
    
    @staticmethod
