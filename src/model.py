f96ebdcab582e922202e2e0730b0971caec11ec5
Fernando Lopez <fer.neutron@gmail.com>
2019-12-03 00:01:10 -0600

    initializing VGAE model

--- src/model.py
+++ src/model.py
@@ -1,10 +1,33 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-class VGAE:
+from torch.autograd import Variable
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class VGAE(nn.Module):
    def __init__(self, **kwargs):
-      self.train_adj = kwargs['train_adj']
-      self.normalized = kwargs['normalized']
-      self.test_edges = kwargs['test_edges']
-      self.false_edges = kwargs['false_edges']
+      super(VGAE, self).__init__()
+      self.num_neurons = kwargs['num_neurons']
+      self.num_features = kwargs['num_features']
+      self.embedding_size = kwargs['embedding_size']
+      self.learning_rate = kwargs['learning_rate']
       
+      self.w_0_mu = Variable(self.num_features, self.num_neurons)
+      self.b_0_mu = Variable(self.num_neurons)
+      self.w_1_mu = Variable(self.num_neurons, self.embedding_size)
+      self.b_1_mu = Variable(self.embedding_size)
+      
+      self.w_0_sigma = Variable(self.num_features, self.num_neurons)
+      self.b_0_sigma = Variable(self.num_neurons)
+      self.w_1_sigma = Variable(self.num_neurons, self.embedding_size)
+      self.b_1_sigma = Variable(self.embedding_size)
+       
+      
+   def forward(self, adjacency, norm_adj):
+      pass
+   
+   def encode(self):
+      pass      
       
