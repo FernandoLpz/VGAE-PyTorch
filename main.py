5987338f1aa80e4eb153dab72295511a58f4bdee
Fernando Lopez <fer.neutron@gmail.com>
2019-12-02 21:28:19 -0600

    Add train_test_split function

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
    
    graph_file = 'benchmarks/fb-pages-food/fb-pages-food_edges.csv'
    
-   pg = PrepareGraph(file=graph_file, test_size=0.2)
+   pg = PrepareGraph(file=graph_file, test_size=0.1)
--- src/utils.py
+++ src/utils.py
@@ -1,40 +1,74 @@
-import networkx as nx
 import numpy as np
+import pandas as pd
+import networkx as nx
+
 from random import randint
 
 class PrepareGraph:
    def __init__(self, **kwargs):
-      self.Graph = PrepareGraph.load_graph(kwargs['file'])
-      self.adjacency = nx.adjacency_matrix(self.Graph)
-      self.train, self.test = PrepareGraph.train_test_split(self.adjacency.copy(), kwargs['test_size'])
+      self.adjacency, self.edges = PrepareGraph.load_adjacency_matrix(kwargs['file'])
+      self.train_adj, self.test_edges, self.false_edges = PrepareGraph.train_test_split(self.adjacency, kwargs['test_size'], self.edges)
       
    @staticmethod
-   def train_test_split(adjacency, test_size):
+   def train_test_split(adjacency, test_size, edges):
+      
+      test_edges = list()
       false_edges = list()
       
-      test_items = round(adjacency.shape[0] * test_size)
+      train_adj = adjacency.copy()
       
-      idx_graph = np.argwhere(adjacency.toarray() == 1)
-      idx_graph = [tuple(edge) for edge in idx_graph]
+      num_test_edges = round(len(edges) * test_size)
       
-      while len(false_edges) < test_items:
-         row = randint(0, adjacency.shape[0])
-         col = randint(0, adjacency.shape[0])
+      # Create false edges
+      while len(false_edges) < num_test_edges:
+         row = randint(0, train_adj.shape[0] - 1)
+         col = randint(0, train_adj.shape[0] - 1)
          
-         false_edge = tuple([row, col])
-         
-         if false_edge not in idx_graph:
-            if false_edge not in false_edges:
-               false_edges.append(false_edge)
+         if row != col:
+            if tuple([row, col]) not in edges:
+               if tuple([col, row]) not in edges:
+                  if tuple([row, col]) not in false_edges:
+                     if tuple([col, row]) not in false_edges:
+                        false_edges.append(tuple([row, col]))
       
+      # Create test edges
+      idx_test = list()
+      while len(idx_test) < num_test_edges:
+         idx = randint(0, len(edges))
+         if idx not in idx_test:
+            idx_test.append(idx)
+            
+      for idx in idx_test:
+         test_edges.append(edges[idx])
+         adjacency[edges[idx][0], edges[idx][1]] = 0
+         adjacency[edges[idx][1], edges[idx][0]] = 0
       
-      
-      return 1,2
+      return train_adj, test_edges, false_edges
       
    
    @staticmethod
-   def load_graph(file):
-      Graph = nx.Graph()
-      graph_file = open(file, 'r')
+   def load_adjacency_matrix(file):
+ 
+      # Get the maximum number of entities
+      def max_id(list_edges):
+         maximum = 0
+         for edge in list_edges:
+            if edge[0] > maximum:
+               maximum = edge[0]
+            if edge[1] > maximum:
+               maximum = edge[1]
+         return maximum
+      
+      # Load csv file
+      edges = pd.read_csv(file, header=None)
+      edges = edges.values
+      edges = [tuple(edge) for edge in edges]
+      num_entities = max_id(edges)
+      
+      # Build adjacency matrix
+      adjacency = np.zeros((num_entities + 1, num_entities + 1), dtype=int)
+      for edge in edges:
+         adjacency[edge[0], edge[1]] = 1
+         adjacency[edge[1], edge[0]] = 1
 
-      return nx.parse_edgelist(graph_file, delimiter=',', create_using=Graph)
+      return adjacency, edges
