e1ded0099b02e351a27f837e1901d0ad2332c62f
Fernando Lopez <fer.neutron@gmail.com>
2019-12-07 00:02:02 -0600

    fix bugs

--- main.py
+++ main.py
@@ -4,48 +4,62 @@ import torch.nn.functional as F
 from src import PrepareGraph
 from src import VGAE
 
-
 def precision(x_hat, x_true):
-      tp, true = 0, 0
-      for i in range(x_true.shape[0]):
-            for j in range(x_true.shape[0]):
-                  if x_true[i][j] == 1:
-                        true += 1
-                        if x_hat[i][j] > 0.5:
-                              tp += 1
-      return tp / true
+   tp, true = 0, 0
+   for i in range(x_true.shape[0]):
+      for j in range(x_true.shape[0]):
+         if x_true[i][j] == 1:
+            true += 1
+            if x_hat[i][j] > 0.5:
+               tp += 1
+   return tp/true
+
+def accuracy(x_hat, test_edges, false_edges, node_to_idx):
+   
+   tp = 0
+   for edge in test_edges:
+      if x_hat[node_to_idx[edge[0]]][node_to_idx[edge[1]]] > 0.5:
+         tp += 1
+   
+   f = 0   
+   for false in false_edges:
+      if x_hat[node_to_idx[false[0]]][node_to_idx[false[1]]] <0.5:
+         f+=1
+         
+   acc = tp /len(test_edges)
+   return acc
 
 if __name__ == '__main__':
    
    graph_file = 'benchmarks/fb-pages-food/fb-pages-food_edges.csv'
-   # graph_file = 'benchmarks/generic/generic.csv'
-   
-   # Load raw files and transform them into adjacency matrix
-   pg = PrepareGraph(file=graph_file, test_size=0.2)
-   
-   norm = pg.adjacency.shape[0] * pg.adjacency.shape[0] / float((pg.adjacency.shape[0] * pg.adjacency.shape[0] - pg.adjacency.sum()) * 2)
-   
-   train_adj = torch.FloatTensor(pg.train_adj)
-   normalized = torch.FloatTensor(pg.normalized)
-   x_features = torch.FloatTensor(pg.x_features)
+	#graph_file = 'benchmarks/generic/generic.csv'
+	
+	# Load raw files and transform them into adjacency matrix
+   pg = PrepareGraph(file=graph_file, test_size=0.1)
+	
+   train_adj = torch.Tensor(pg.train_adj)
+   normalized = torch.Tensor(pg.normalized)
+   x_features = torch.Tensor(pg.x_features)
 
    # Instatiate VGAE model
-   vgae = VGAE(num_neurons=128, num_features=pg.adjacency.shape[0], embedding_size=128)
-   optimizer = torch.optim.Adam(vgae.parameters(), lr=0.1)
-   for epoch in range(50):
-   
+   vgae = VGAE(num_neurons=16, num_features=pg.adjacency.shape[0], embedding_size=12)
+   optimizer = torch.optim.Adam(vgae.parameters(), lr=0.01)
+	
+   for epoch in range(150):
+	
       vgae.train()
-      
+
       x_hat = vgae(train_adj, normalized, x_features)
-      loss = norm * F.binary_cross_entropy_with_logits(x_hat, train_adj)
-      kl_divergence = F.kl_div(vgae.GCN_mu, vgae.GCN_sigma)
+      optimizer.zero_grad()
+
+      loss = F.binary_cross_entropy(x_hat, train_adj)
+      kl_divergence = 0.5/ x_hat.size(0) * (1 + 2*vgae.GCN_sigma - vgae.GCN_mu**2 - torch.exp(vgae.GCN_sigma)).sum(1).mean()
       loss -= kl_divergence
-      
+
       loss.backward()
       optimizer.step()
-      optimizer.zero_grad()
       
-      prec = precision(x_hat, pg.train_adj)
-      print('Epoch: ', epoch + 1, 'loss: ', loss.item(), 'precision: ', prec)
-      
-      
+      prec = precision(x_hat, train_adj)
+      acc = accuracy(x_hat, pg.test_edges, pg.false_edges, pg.node_to_id)
+
+      print('Epoch: ', epoch + 1, 'loss: ', loss.item(), 'precision: ', prec, 'acc: ', acc)
--- src/model.py
+++ src/model.py
@@ -14,10 +14,9 @@ class VGAE(nn.Module):
       self.num_neurons = kwargs['num_neurons']
       self.num_features = kwargs['num_features']
       self.embedding_size = kwargs['embedding_size']
-      self.dropout = torch.nn.Dropout(0.5)
 
-      #self.w_0 = torch.nn.Parameter(torch.randn(self.num_features, self.num_neurons, requires_grad=True))
       self.w_0 = VGAE.glorot_init(self.num_features, self.num_neurons)
+      #self.w_0 = torch.nn.Parameter(torch.randn(self.num_features, self.num_neurons, requires_grad=True))
       self.b_0 = torch.nn.Parameter(torch.randn(self.num_neurons, requires_grad=True))
       
       self.w_1_mu = VGAE.glorot_init(self.num_neurons, self.embedding_size)
@@ -31,10 +30,9 @@ class VGAE(nn.Module):
       # torch.nn.init.normal_(self.w_0)
       # torch.nn.init.normal_(self.w_1_mu)
       # torch.nn.init.normal_(self.w_1_sigma)
-   
-   @staticmethod 
-   def glorot_init(input_dim, output_dim):
       
+   @staticmethod
+   def glorot_init(input_dim, output_dim):
       init_range = np.sqrt(6.0/(input_dim + output_dim))
       initial = torch.rand(input_dim, output_dim)*2*init_range - init_range
       
@@ -43,11 +41,10 @@ class VGAE(nn.Module):
    def encode(self, adjacency, norm_adj, x_features):
       
       hidden_0 = torch.relu(torch.add(torch.matmul(torch.matmul(norm_adj, x_features), self.w_0), self.b_0))
-      # hidden_0 = self.dropout(hidden_0)
       self.GCN_mu = torch.add(torch.matmul(torch.matmul(norm_adj, hidden_0), self.w_1_mu), self.b_1_mu)
       self.GCN_sigma = torch.exp(torch.add(torch.matmul(torch.matmul(norm_adj, hidden_0), self.w_1_sigma), self.b_1_sigma))
 
-      z = self.GCN_mu + ( torch.randn(adjacency.size(0), self.embedding_size) * self.GCN_sigma )
+      z = torch.randn(adjacency.size(0), self.embedding_size) * self.GCN_sigma + self.GCN_mu
       
       return z
    
